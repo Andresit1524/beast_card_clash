51f4199466c22d09358601f99852c92470bcf217
richirockie15 <richity99@gmail.com>
2026-02-06 15:08:01 -0500

    Mondongo

--- ui/start_menu/start_menu_buttons.gd
+++ ui/start_menu/start_menu_buttons.gd
@@ -5,12 +5,12 @@ extends VBoxContainer
 
 # Botón de jugar: carga la escena de tutorial
 func _on_play_button_pressed() -> void:
-    SceneManager.to_tutorial_scene()
+	SceneManager.to_tutorial_scene()
 
 # Botón de créditos: carga la escena de créditos
 func _on_credits_button_pressed() -> void:
-    SceneManager.to_credits_scene()
+	SceneManager.to_credits_scene()
 
 # Botón de salir: sale del juego de inmediato
 func _on_quit_button_pressed() -> void:
-    get_tree().quit()
+	get_tree().quit()
