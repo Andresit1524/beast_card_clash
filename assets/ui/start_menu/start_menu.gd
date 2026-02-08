8c45068049369e9c02f1b001a6dec0fd097f2a21
Andrés López <andreslopez01182009@gmail.com>
2026-02-07 19:06:32 -0500

    Primer versión del selector de personajes

--- assets/ui/start_menu/start_menu.gd
+++ assets/ui/start_menu/start_menu.gd
@@ -4,10 +4,12 @@ extends Node
 func _ready() -> void:
     MusicManager.play_music("start_menu")
 
-## ! Botón de jugar: no definido
+## Botón de jugar: se va al selector de personaje o a la escena de juego
 func _on_play_button_pressed() -> void:
-    push_warning("No hay escena de juego")
-    pass
+    if FlagsManager.get_flag("character_selected"):
+        push_warning("No hay escena de juego")
+    else:
+        SceneManager.change_to_scene("character_selector")
 
 ## Botón de créditos: carga la escena de créditos
 func _on_credits_button_pressed() -> void:
