0768b6fafffef24e5658173f8a861b1362e78e8a
Andrés López <andreslopez01182009@gmail.com>
2026-06-01 15:28:03 -0500

    Versión de juego en el menú

--- assets/ui/start_menu/start_menu.gd
+++ assets/ui/start_menu/start_menu.gd
@@ -1,15 +1,22 @@
 extends Node
 
 
-# Reproduce la música de inicio al iniciar el juego
+## Etiqueta de la versión actual
+@onready var version: Label = %Version
+
+
 func _ready() -> void:
 	MusicManager.play_music("menu_loop")
+
+	# Actualiza la versión automáticamnte
+	version.text = "v%s" % ProjectSettings.get_setting("application/config/version", "[desconocida]")
 
 
 ## Botón de jugar: se va al selector de personaje o a la escena de juego
 func _on_play_button_pressed() -> void:
 	if FlagsManager.get_flag("character_selected"):
-		push_warning("No hay escena de juego")
+		push_warning("No hay escena de mapa. Pasando a partida rápida")
+		SceneManager.change_to_scene("battle")
 	else:
 		SceneManager.change_to_scene("skin_selector")
 
@@ -24,11 +31,11 @@ func _on_credits_button_pressed() -> void:
 	SceneManager.change_to_scene("credits")
 
 
-## Botón de salir: sale del juego de inmediato
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
 ## Botón de tutorial: abre el tutorial
 func _on_tutorial_button_pressed() -> void:
 	SceneManager.change_to_scene("tutorial")
+
+
+## Botón de salir: sale del juego de inmediato
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
