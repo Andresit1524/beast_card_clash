beafb2cb7960adbb6c7dfef59e5664f7cc930562
Andrés López <andreslopez01182009@gmail.com>
2026-06-13 18:37:01 -0500

    Música de final + Docs fuera

--- assets/battle/states/end.gd
+++ assets/battle/states/end.gd
@@ -3,6 +3,8 @@ class_name BattleEnd extends BattleState
 
 
 func start() -> void:
+	MusicManager.stream_paused = true
+
 	# Si perdimos, completa el ranking primero
 	if battle_data.we_lose(): battle_data.lose_remaining()
 
